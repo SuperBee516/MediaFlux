ec73f3faa5545a7d49e7d4e20cb21d03cdf78f6b
SuperBee1776 <132794615+SuperBee516@users.noreply.github.com>
2026-01-05 20:20:01 -0500

    Updated version: 0.5.5

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1121,7 +1121,7 @@ namespace Encode
 
             MainMenuStrip = menuStrip1;
 
-            Text = "GoEncode v0.5.4";
+            Text = "GoEncode v0.5.5";
             ClientSize = new Size(800, 760);
         }       
     }
