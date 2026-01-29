839c59644757c159b50581ea759af2e0506e72a2
Gregg Hirschberg <ghirsch516@live.com>
2026-01-28 21:37:11 -0500

    Version update

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1205,7 +1205,7 @@ namespace Encode
 
             MainMenuStrip = menuStrip1;
 
-            Text = "GoEncode v0.6.1";
+            Text = "GoEncode v0.6.2";
             ClientSize = new Size(800, 760);
         }
     }
